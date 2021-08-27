5ee56cae74c9e7c3ea7274967f96d0c58c8e3fdb
Susi <de_vasconcelos@stud.uni-heidelberg.de>
2021-08-27 10:25:41 +0100

    finally found good pid values

--- ROS_Integration/firmware_rewrite/firmware_rewrite.ino
+++ ROS_Integration/firmware_rewrite/firmware_rewrite.ino
@@ -1,4 +1,12 @@
+// ROS Headers
+#include <ros.h>
+#include <geometry_msgs/Pose2D.h>
+#include <std_msgs/Float32.h>
+#include <geometry_msgs/Twist.h>
+
 #include <Encoder.h>
+#include <SharpIR.h>
+#include <Average.h>
 
 // encoder pins
 const byte encL1 = 2;
@@ -15,6 +23,45 @@ const byte leftMotorToggle = 6;
 // initialize encoders
 Encoder encR(encL1, encL2);
 Encoder encL(encR2, encR1);
+
+// sensor pin numbers
+const byte sensorLeftPin  = A3;
+const byte sensorFrontPin = A4;
+const byte sensorRightPin = A2;
+
+// object to get distance from sensors
+SharpIR sensorLeft (SharpIR::GP2Y0A21YK0F, sensorLeftPin );
+SharpIR sensorFront(SharpIR::GP2Y0A21YK0F, sensorFrontPin);
+SharpIR sensorRight(SharpIR::GP2Y0A21YK0F, sensorRightPin);
+
+// We take the average of the ten last sensor outputs
+Average<float> avgLeft(10);
+Average<float> avgFront(10);
+Average<float> avgRight(10);
+
+// ROS
+ros::NodeHandle nh;
+
+// Publish robot pose
+geometry_msgs::Pose2D pose_for_ros;
+ros::Publisher pose_pub("pose", &pose_for_ros);
+
+// Publish sensor ranges
+std_msgs::Float32 front_distance, right_distance, left_distance;
+ros::Publisher front_distance_pub("front_distance", &front_distance);
+ros::Publisher right_distance_pub("right_distance", &right_distance);
+ros::Publisher left_distance_pub ("left_distance" , &left_distance );
+
+// Subscribe to cmd_vel to get velocity
+float linear_vel  = 0.;
+float angular_vel = 0.;
+
+void cmd_vel_callback(const geometry_msgs::Twist& msg){
+  linear_vel = msg.linear.x;
+  angular_vel = msg.angular.z;
+}
+
+ros::Subscriber<geometry_msgs::Twist> cmd_vel_sub("cmd_vel", cmd_vel_callback);
 
 float R = 0.015;
 float B = 0.095;
@@ -33,14 +80,6 @@ void poseUpdate(float NL, float NR, float* pose, float deltaT, float* realVels){
     pose[0] = pose[0] + v * cos(theta) * deltaT;
     pose[1] = pose[1] + v * sin(theta) * deltaT;
     pose[2] = theta;
-
-    Serial.print(pose[0]);
-    Serial.print(" ");
-    Serial.print(pose[1]);
-    Serial.print(" ");
-    Serial.print(pose[2]);
-    Serial.print(" ");
-    Serial.println();
   }
 
 // converts lineaer and angular velocities to wheels angular velocities
@@ -78,7 +117,34 @@ void pid(float Kp, float Ki, float Kd, float* prop_error, float* deriv_error, fl
 long unsigned currentTime;
 // Arduino
 void setup() {
-  Serial.begin(9600);
+  // Output
+  pinMode(rightMotorSpeed, OUTPUT);
+  pinMode(rightMotorToggle, OUTPUT);
+  pinMode(leftMotorSpeed, OUTPUT);
+  pinMode(leftMotorToggle, OUTPUT);
+   
+  // Input
+  pinMode(encL1,INPUT);
+  pinMode(encL2,INPUT);
+  pinMode(encR1,INPUT);
+  pinMode(encR2,INPUT);
+  pinMode(sensorLeftPin,INPUT);
+  pinMode(sensorFrontPin,INPUT);
+  pinMode(sensorRightPin,INPUT);
+
+  nh.initNode();
+  //nh.getHardware()->setBaud(57600);
+  
+  // The topics that robot publishes:
+  nh.advertise(pose_pub);
+  
+  nh.advertise(front_distance_pub);
+  nh.advertise(right_distance_pub);
+  nh.advertise(left_distance_pub);
+
+  // The topics that robot subscribes to:
+  nh.subscribe(cmd_vel_sub);
+  
   currentTime = millis();
 }
 
@@ -100,7 +166,7 @@ float deltaT = 0.1;
 
   
 void loop() {
-    cmd_vel2wheels(0., 0, desiredWheelVels);
+    cmd_vel2wheels(linear_vel, angular_vel, desiredWheelVels); //anguar: 0 to 2, linear: 0 to 0.1
     if (millis() - currentTime > 100){ // 100ms
       // output
       currentPulses[0] = encL.read();
@@ -117,7 +183,7 @@ void loop() {
       getDerivError(propErrorOld, propErrorNew, derivErrorNew);
       getIntegralError(integralErrorOld, propErrorNew, integralErrorNew);
   
-      pid(67, 0., 0, propErrorNew, derivErrorNew, integralErrorNew, deltaT, gain); 
+      pid(20, 100, 0, propErrorNew, derivErrorNew, integralErrorNew, deltaT, gain); 
 
       if (gain[0] > 0) digitalWrite(leftMotorToggle, LOW);
       else digitalWrite(leftMotorToggle, HIGH);
@@ -130,21 +196,36 @@ void loop() {
       if (gain[1] < -255) gain[1] = -255;
       analogWrite(leftMotorSpeed, abs((int)gain[0]));
       analogWrite(rightMotorSpeed, abs((int)gain[1]));
-      Serial.print("gain");
-      Serial.print(gain[0]);
-      Serial.print(" ");
-      Serial.print(gain[1]);
-      Serial.println();
-      Serial.println();
+
+      pose_for_ros.x = pose[0];
+      pose_for_ros.y = pose[1];
+      pose_for_ros.theta = pose[2];
+      pose_pub.publish(&pose_for_ros);
+  
+      avgLeft.push(sensorLeft.getDistance());
+      avgFront.push(sensorFront.getDistance());
+      avgRight.push(sensorRight.getDistance());
+      
+      front_distance.data = avgLeft.mean();
+      front_distance_pub.publish(&front_distance);
+      
+      right_distance.data = avgFront.mean();
+      right_distance_pub.publish(&right_distance);
+      
+      left_distance.data = avgRight.mean();
+      left_distance_pub.publish(&left_distance);
+      
       // updates
       oldPulses[0] = currentPulses[0];
       oldPulses[1] = currentPulses[1];
       
       propErrorOld[0] = propErrorNew[0];
       propErrorOld[1] = propErrorNew[1];
+      
       integralErrorOld[0] = integralErrorNew[0];
       integralErrorOld[1] = integralErrorNew[1];    
       
       currentTime = millis();
     }
+    nh.spinOnce();
 }
